e39ff730355a31cc5aca665c612831f002603846
NTD4503 <nguyendat12501@gmail.com>
2024-01-24 21:52:30 +0700

    create new workspace & get detail

--- src/util/APIConfig.js
+++ src/util/APIConfig.js
@@ -2,6 +2,7 @@ class APIConfig {
     // Auth
     static SIGNUP = "/signup";
     static GET_USER_INFO = "/get-user-info";
+    static SEARCH_USER_EMAIL = "/search-user-email";
 
     // Task
     static CREATE_TASK = "/create-task";
@@ -11,6 +12,14 @@ class APIConfig {
     static GET_EVENT = "/get-event";
     static EDIT_EVENT = "/edit-event";
     static DELETING_EVENT = "/delete-event";
+
+    // Workspace
+    static CREATE_WORKSPACE = "/create-workspace";
+    static GET_WORKSPACE_BY_USERID = "/get-workspace-by-userid";
+    static GET_DETAIL_WORKSPACE = "/get_detail-workspace";
+
+    // Project
+    static CREATE_PROJECT = "/create-project";
 }
 
 module.exports = APIConfig;
